f30ca296d54d3cc7522c6e67eedc26da584345a4
malwinakochman <s223011@student.uek.krakow.pl>
2025-05-30 22:38:13 +0200

    Initial Commit

---
+++ finder/finderApp.swift
@@ -0,0 +1,17 @@
+//
+//  finderApp.swift
+//  finder
+//
+//  Created by Malwina Kochman on 30/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct finderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ finderTests/finderTests.swift
@@ -0,0 +1,16 @@
+//
+//  finderTests.swift
+//  finderTests
+//
+//  Created by Malwina Kochman on 30/05/2025.
+//
+
+import Testing
+
+struct finderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
